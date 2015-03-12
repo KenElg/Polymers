85d516f5e3c268ff78f5a01a3af15aab45c8d555
misrbechan <misrbechan@gmail.com>
2015-03-12 16:34:18 +0100

    test phase

--- rosenbluth.py
+++ rosenbluth.py
@@ -21,7 +21,7 @@ def Addbead(R,Weight,L,anglenum,U):
     startang=np.random.random() 
     for i in range (anglenum):
         theta = startang + i* 2 * pi/anglenum
-        R[L+1]=[R[L,1]+cos(theta),R[L,2]+sin(theta)]
+        R[L+1]=[R[L,0]+cos(theta),R[L,1]+sin(theta)]
         compare[i,:]=R[L+1]
         E=ljcalc(R,L+1,U,N)
         w[i]=exp(-E)
@@ -36,3 +36,5 @@ def Addbead(R,Weight,L,anglenum,U):
     if L < N:
         Addbead(R,Weight,L+1,anglenum,0)
     return R, Weight
+A = np.zeros((N,2))    
+A,W = Addbead(R,1,1,anglenum,0)
