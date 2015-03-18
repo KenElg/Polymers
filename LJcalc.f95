1fc4dc04169c60dee926771dba3d1c9ed8b5d9bf
misrbechan <misrbechan@gmail.com>
2015-03-18 16:46:49 +0100

    added eps & sig to ensure E is not extremely small

--- LJcalc.f95
+++ LJcalc.f95
@@ -7,13 +7,14 @@ real(8), intent(in) :: R(n, 2)
 real(8), intent(inout) :: U
 !f2py intent(in, out) U
 real(8) :: dr(2), dr2
-real(8), parameter :: rmax = 3.2_8
+real(8), parameter :: eps = 0.25
+real (8), parameter :: sig = 0.8
 integer :: i, j
 do i = 0, Pnum
     do j = 1, i-1
         dr = R(i,:) - R(j,:)
         dr2 = sum(dr**2)
-        U=4*(1/dr2**6 - 1/dr2**3)        
+        U=4*eps*(sig**12/dr2**6 - sig**6/dr2**3)        
     end do
 end do
 end subroutine
