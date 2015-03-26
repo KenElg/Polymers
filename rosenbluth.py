f2725cf781a3ea0ae707eb3dc18e0db8a511592b
misrbechan <misrbechan@gmail.com>
2015-03-26 15:22:59 +0100

    rosenbluth cleaned up

--- rosenbluth.py
+++ rosenbluth.py
@@ -11,65 +11,68 @@ L = 2
 T = 1
 polsize = 100  #max polymer size
 polpop = 10    # polymer population
-R2 = np.zeros((polpop,polsize))
-num=0
-for j in range (polpop):
-    R = np.zeros ((polsize,2))
-    R[1,:] = [1,0]
-    anglenum=6
+    
+def Addbead(beadpos,Weight,L,anglenum,U):
+    startang=np.random.random()
     w = np.zeros((1,anglenum))
     compare=np.zeros((anglenum,2))
-    def Addbead(R,Weight,L,anglenum,U):
-        startang=np.random.random() 
-        for i in range (anglenum):
-            theta = startang + i* 2 * pi/anglenum
-            R[L,:]=[R[L-1,0]+cos(theta),R[L-1,1]+sin(theta)]
-            compare[i,:]= R[L,:]
-            E=ljcalc(R,L+1,U,polsize)
-            w[0,i]=exp(-E)
-#            print E
-        W = np.sum(w)
-        if W ==0:
-            print "Polymer number=", j, "Beadnumber =", L
-        else:
-            Track = np.cumsum(w/W)
-            Test=np.random.random()
-            for i in range (len(Track)):   
-                if Test < Track[i]:      
-                    num = i   #check in which of the rows of track our test falls
-                    Test=Test+1 # Ensures we only have 1 found value
-                    # print num
-            R[L]=compare[num,:]
+    for i in range (anglenum):
+        theta = startang + i* 2 * pi/anglenum
+        beadpos[L,:]=[beadpos[L-1,0]+cos(theta),beadpos[L-1,1]+sin(theta)]
+        compare[i,:]= beadpos[L,:]
+        E=ljcalc(beadpos,L+1,U,polsize)
+        w[0,i]=exp(-E)
+    W = np.sum(w)
+    if W ==0:
+        print  "Last Beadnumber =", L
+    else:
+        Track = np.cumsum(w/W)
+        Test=np.random.random()
+        for i in range (len(Track)):   
+            if Test < Track[i]:      
+                num = i   #check in which of the rows of track our test falls
+                Test=Test+1 # Ensures we only have 1 found value
+        beadpos[L]=compare[num,:]
 #            print R[L,:]
-            R2[j,L] = np.sum(R[L,:]**2) # end to end distance squared 
-            Weight=Weight*W
-            if L < polsize-1:
-                
-                Addbead(R,Weight,L+1,anglenum,0)
-                       
-        return R, Weight, L, R2
-    R, Weight, L, R2 = Addbead(R,1,L,anglenum,0)
+#        end2end2[j,L] = np.sum(beadpos[L,:]**2) # end to end distance squared 
+        Weight=Weight*W
+        if L < polsize-1:          
+            Addbead(beadpos,Weight,L+1,anglenum,0)                   
+        return beadpos, Weight, L
 
-R2[np.where(R2==0)] = np.nan 
-#print R2
-R2mean = np.nanmean(R2,axis=0)
-R2var = np.nanvar(R2,axis=0,dtype=float)/(np.arange(polsize)**(0.5))
 
-plt.xscale("log", nonposx='clip')
-plt.yscale("log", nonposy='clip')
-plt.errorbar(np.arange(3,polsize+1),R2mean[2:], R2var[2:],linestyle = 'none',marker='x')
 
-plt.xlim([2,250])
-plt.xlabel("N")
-plt.ylabel("R^2")
-#plt.plot(R[:,0],R[:,1])
+#def Calcendtoend(beadpos):
+#    end2end2=np.zeros((1,len(beadpos))) #end to end squared
+#    for j in range(2,len(beadpos)):
+#        end2end2[0,j] = np.sum(beadpos[j,:]**2) # end to end distance squared 
+#    return end2end2
+#
+#
+#R2[np.where(R2==0)] = np.nan 
+##print R2
+#R2mean = np.nanmean(R2,axis=0)
+#R2var = np.nanvar(R2,axis=0,dtype=float)/(np.arange(polsize)**(0.5))
+#
+#plt.xscale("log", nonposx='clip')
+#plt.yscale("log", nonposy='clip')
+#plt.errorbar(np.arange(3,polsize+1),R2mean[2:], R2var[2:],linestyle = 'none',marker='x')
+#
+#plt.xlim([2,250])
+#plt.xlabel("N")
+#plt.ylabel("R^2")
+##plt.plot(R[:,0],R[:,1])
+#
+#a = 1
+#x = np.arange(polsize)
+#fit = a*(x-1)**1.5
+#plt.plot(x,fit)
+#plt.show()
 
-a = 1
-x = np.arange(polsize)
-fit = a*(x-1)**0.75
-plt.plot(x,fit)
-plt.show()
-
+anglenum=6
+beadpos = np.zeros ((polsize,2))
+beadpos[1,:] = [1,0]
+beadpos, Weight, L = Addbead(beadpos,1,L,anglenum,0)
 plt.figure()
-plt.plot(R[:,0],R[:,1])
+plt.plot(beadpos[:,0],beadpos[:,1])
 plt.show
