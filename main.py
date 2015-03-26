e38b4da49a0a6b8350a5ac2c66dd845f67b39609
KenElg <kenneth.elgin@gmail.com>
2015-03-26 16:39:34 +0100

    cleaned up rosenbluth and moved plotting to main file

---
+++ main.py
@@ -0,0 +1,44 @@
+import numpy as np
+import math
+from LJcalc import ljcalc
+import matplotlib.pyplot as plt 
+import rosenbluth
+pi=math.pi
+cos=math.cos
+sin=math.sin
+exp=math.exp
+L = 2
+T = 1
+polsize = 100  #max polymer size
+polpop = 10    # polymer population
+              
+#
+#R2[np.where(R2==0)] = np.nan 
+##print R2
+#R2mean = np.nanmean(R2,axis=0)
+#R2var = np.nanvar(R2,axis=0,dtype=float)/(np.arange(polsize)**(0.5))
+#
+#plt.xscale("log", nonposx='clip')
+#plt.yscale("log", nonposy='clip')
+#plt.errorbar(np.arange(3,polsize+1),R2mean[2:], R2var[2:],linestyle = 'none',marker='x')
+#
+#plt.xlim([2,250])
+#plt.xlabel("N")
+#plt.ylabel("R^2")
+##plt.plot(R[:,0],R[:,1])
+#
+#a = 1
+#x = np.arange(polsize)
+#fit = a*(x-1)**1.5
+#plt.plot(x,fit)
+#plt.show()
+
+anglenum=6
+beadpos = np.zeros ((polsize,2))
+beadpos[1,:] = [1,0]
+beadpos, Weight, L = rosenbluth.Addbead(beadpos,1,L,anglenum,0,polsize)
+endtoend2=rosenbluth.Calcendtoend2(beadpos)
+Rad=rosenbluth.RadofGyr(beadpos)
+plt.figure()
+plt.plot(beadpos[:,0],beadpos[:,1])
+plt.show  
--- rosenbluth.py
+++ rosenbluth.py
@@ -7,12 +7,8 @@ pi=math.pi
 cos=math.cos
 sin=math.sin
 exp=math.exp
-L = 2
-T = 1
-polsize = 100  #max polymer size
-polpop = 10    # polymer population
     
-def Addbead(beadpos,Weight,L,anglenum,U):
+def Addbead(beadpos,Weight,L,anglenum,U,polsize):
     startang=np.random.random()
     w = np.zeros((1,anglenum))
     compare=np.zeros((anglenum,2))
@@ -37,42 +33,26 @@ def Addbead(beadpos,Weight,L,anglenum,U):
 #        end2end2[j,L] = np.sum(beadpos[L,:]**2) # end to end distance squared 
         Weight=Weight*W
         if L < polsize-1:          
-            Addbead(beadpos,Weight,L+1,anglenum,0)                   
+            Addbead(beadpos,Weight,L+1,anglenum,0,polsize)                   
         return beadpos, Weight, L
 
 
 
-#def Calcendtoend(beadpos):
-#    end2end2=np.zeros((1,len(beadpos))) #end to end squared
-#    for j in range(2,len(beadpos)):
-#        end2end2[0,j] = np.sum(beadpos[j,:]**2) # end to end distance squared 
-#    return end2end2
-#
-#
-#R2[np.where(R2==0)] = np.nan 
-##print R2
-#R2mean = np.nanmean(R2,axis=0)
-#R2var = np.nanvar(R2,axis=0,dtype=float)/(np.arange(polsize)**(0.5))
-#
-#plt.xscale("log", nonposx='clip')
-#plt.yscale("log", nonposy='clip')
-#plt.errorbar(np.arange(3,polsize+1),R2mean[2:], R2var[2:],linestyle = 'none',marker='x')
-#
-#plt.xlim([2,250])
-#plt.xlabel("N")
-#plt.ylabel("R^2")
-##plt.plot(R[:,0],R[:,1])
-#
-#a = 1
-#x = np.arange(polsize)
-#fit = a*(x-1)**1.5
-#plt.plot(x,fit)
-#plt.show()
-
-anglenum=6
-beadpos = np.zeros ((polsize,2))
-beadpos[1,:] = [1,0]
-beadpos, Weight, L = Addbead(beadpos,1,L,anglenum,0)
-plt.figure()
-plt.plot(beadpos[:,0],beadpos[:,1])
-plt.show
+def Calcendtoend2(beadpos):
+    end2end2=np.zeros((1,len(beadpos))) #end to end squared
+    for j in range(2,len(beadpos)):
+        end2end2[0,j] = np.sum(beadpos[j,:]**2) # end to end distance squared 
+    return end2end2
+    
+def RadofGyr(beadpos):
+    RG2=np.zeros((1,len(beadpos))) #end to end squared    
+    for j in range(2,len(beadpos)):
+        R0=np.zeros((1,2))
+        Calc=0
+        R0[0,0]=(1.0/j)*np.sum(beadpos[0:j,0])
+        R0[0,1]=(1.0/j)*np.sum(beadpos[0:j,1])
+        for k in range(j):
+            Calc=Calc+(beadpos[k,:]-R0)**2
+#            print Calc
+        RG2[0,j]=(1.0/j)*np.sum(Calc)
+    return RG2
